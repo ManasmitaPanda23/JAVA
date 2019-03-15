4716bc2d9890e609c17bfcee7c28126a2dd117b0
ManasmitaPanda23 <48487835+ManasmitaPanda23@users.noreply.github.com>
2019-03-15 16:49:41 +0530

    Add files via upload

---
+++ InitBlocks.java
@@ -0,0 +1,18 @@
+package objectclass;
+
+public class InitBlocks {
+	static int i;
+
+	static {
+		System.out.println("Static Init Block");
+	}
+
+	{
+		System.out.println("Init Block");
+	}
+
+	public InitBlocks() {
+		System.out.println("Constructor");
+	}
+
+}
---
+++ TestInitBlocks.java
@@ -0,0 +1,10 @@
+package objectclass;
+
+public class TestInitBlocks {
+public static void main(String[] args) {
+		
+		new InitBlocks();
+		new InitBlocks();
+
+	}
+}
