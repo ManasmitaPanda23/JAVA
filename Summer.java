babf0485bdfe64c6f053f5f264b045851c4f8fe6
ManasmitaPanda23 <48487835+ManasmitaPanda23@users.noreply.github.com>
2019-03-16 16:02:45 +0530

    Add files via upload

---
+++ Summer.java
@@ -0,0 +1,12 @@
+package varrags;
+
+public class Summer {
+	public void findSum(int... nums) {
+		int sum = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		System.out.println("Sum=" + sum);
+	}
+}
---
+++ TestSummer.java
@@ -0,0 +1,10 @@
+package varrags;
+
+public class TestSummer {
+public static void main(String[] args) {
+		
+		Summer s = new Summer();
+		
+		s.findSum(1,2,3,4,5);
+	}
+}
